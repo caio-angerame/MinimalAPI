d1bf17cadad04c6e24cbf9c5ba3a7dbafd4539cb
caio_angerame <caio.angerame2@gmail.com>
2025-10-03 17:24:36 -0300

    Configurando modelo de veículos
    Apresenta a entidade Veiculos, sua interface de serviço e implementação, e a integra ao contexto do BD. Adiciona arquivos de migração para criar a tabela Veiculos no banco de dados. Também aplica atributos obrigatórios nas propriedades Administrador.

---
+++ Dominio/Servicos/VeiculoServico.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Dominio.Entidades;
+using MinimalAPI.Dominio.Interfaces;
+using MinimalAPI.DTOs;
+using MinimalAPI.Infraestrutura.DB;
+
+namespace minimalAPI.Dominio.Servicos;
+
+public class VeiculoServico : IVeiculoServico
+{
+  private readonly DBContexto _contexto;
+  public VeiculoServico(DBContexto contexto)
+  {
+    _contexto = contexto;
+  }
+
+  public void Apagar(Veiculo veiculo)
+  {
+    _contexto.Veiculos.Remove(veiculo);
+    _contexto.SaveChanges();
+  }
+
+  public void Atualizar(Veiculo veiculo)
+  {
+    _contexto.Veiculos.Update(veiculo);
+    _contexto.SaveChanges();
+  }
+
+  public Veiculo? BuscarPorId(int id)
+  {
+    return _contexto.Veiculos.Where(v => v.Id == id).FirstOrDefault();
+  }
+
+  public void Incluir(Veiculo veiculo)
+  {
+    _contexto.Veiculos.Add(veiculo);
+    _contexto.SaveChanges();
+  }
+
+  public List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null)
+  {
+    var query = _contexto.Veiculos.AsQueryable();
+    if (!string.IsNullOrEmpty(nome))
+    {
+      query = query.Where(v => EF.Functions.Like(v.Nome.ToLower(), $"%{nome.ToLower()}%"));
+    }
+
+    int itensPorPagina = 10;
+
+    query = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);  
+
+    return query.ToList();
+  }
+}
--- Migrations/20251003193503_VeiculosMigration.Designer.cs
+++ Migrations/20251003193503_VeiculosMigration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MinimalAPI.Infraestrutura.DB;
 
@@ -10,9 +11,11 @@ using MinimalAPI.Infraestrutura.DB;
 namespace minimalAPI.Migrations
 {
     [DbContext(typeof(DBContexto))]
-    partial class DBContextoModelSnapshot : ModelSnapshot
+    [Migration("20251003193503_VeiculosMigration")]
+    partial class VeiculosMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
