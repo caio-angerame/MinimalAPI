a41a4fc7b6b0279af6d60a4011c5860ba529e244
caio_angerame <caio.angerame2@gmail.com>
2025-10-06 15:47:29 -0300

    GET, POST e tags no endpoints
    Implementação de um POST para criação de veículos, um GET para retornar os veículos inseridos e adição de tags nos endpoints para melhor apresentação no Swagger.

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@ using MinimalAPI.Dominio.Interfaces;
 using minimalAPI.Dominio.Servicos;
 using Microsoft.AspNetCore.Mvc;
 using MinimalAPI.Dominio.ModelViews;
+using MinimalAPI.Dominio.Entidades;
+
+#region Builder
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,21 +26,51 @@ builder.Services.AddDbContext<DBContexto>(options =>
 });
 
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => Results.Json(new Home()));
+#region Home
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-if (administradorServico.Login(loginDTO) != null)
+  if (administradorServico.Login(loginDTO) != null)
     return Results.Ok("Login efetuado com sucesso!");
   else
     return Results.Unauthorized();
-});
+}).WithTags("Administradores");
+#endregion
+
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+  var veiculo = new Veiculo
+  {
+    Nome = veiculoDTO.Nome,
+    Marca = veiculoDTO.Marca,
+    Ano = veiculoDTO.Ano
+  };
+  veiculoServico.Incluir(veiculo);
+
+  return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+}).WithTags("Veículos");
+
+app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
+{
+  var veiculos = veiculoServico.Todos(pagina);
+
+  return Results.Ok(veiculos);
+}).WithTags("Veículos");
+#endregion
+
+#region App
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
 
 
 
